65027f4d856d7d2671303bf82ae81103ae7e6c54
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-05 13:52:00 +0530

    app.js update

--- Express/app.js
+++ Express/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PORT } from './env.js';
+import path from 'path';
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.get("/about", (req, res) => res.send("<h3>Welcome to about page.</h3>"));
 app.get("/about/next", (req, res) => res.send("<h4>Welcome to the next page after about page.</h4>"));
 
 app.get("/URL", (req, res) => {
-    return res.send(` <div class="container">
+  return res.send(` <div class="container">
       <h1>URL Shortner</h1>
       <form id="shorten-form">
         <div class="inputs">
@@ -28,9 +29,28 @@ app.get("/URL", (req, res) => {
     </div>`);
 });
 
+app.get("/", (req, res) => {
+  // console.log(__dirname);
+  // console.log(__filename);
+  // const __filename = new URL(import.meta.url).pathname
+  // console.log(__filename);
+  // console.log(import.meta.url)
+
+//   res.send(` <div class="text-section">
+//     <h1>Never console the logs</h1>
+//     <p> Else logs will console you. </p>
+//     <a href="#"> God Bless you </a>
+// </div>
+// <div class="img">
+// </div>`);
+// });
+const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+res.sendFile(homePagePath);
+});
+
 // const PORT = process.env.PORT || 3000; // $env:VARIABLE_NAME="VALUE"
 app.listen(PORT, () => {
-    console.log(`Server is running at port: ${PORT}`);
+  console.log(`Server is running at port: ${PORT}`);
 });
 
 
