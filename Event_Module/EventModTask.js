31011683b91fdeeeae97fc2a23c70da5ac703e42
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-09 18:10:45 +0530

    Event module task commit

--- Event_Module/EventModTask.js
+++ Event_Module/EventModTask.js
@@ -1,48 +1,54 @@
-/* Objective
-Create a program using Node.js EventEmitter that:
-- Listens for multiple types of user events (e.g., login, logout, purchase, and profile update).
-- Tracks how many times each event is emitted.
-- Logs a summary of all event occurances when a special summary event is triggered.
-
-Requirements :
-- Create atleast four custom events (e.g., user-login, user-logout, user-purchase, profile-update).
-- Emit these events multiple times with different arguements (e.g., username, items purchased).
-- Track and store the count of each event type.
-- Define a summary event that logs a detailed report of how-many times each event was triggered. */
-
+const fs = require("fs");
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
-const eventCounts = {
-    "user-login": 0,
-    "user-logout": 0,
-    "user-purchase": 0,
-    "profile-update": 0,
+const filePath = "./EventModTask.json";
+
+let eventCounts = {};
+if (fs.existsSync(filePath)) {
+    eventCounts = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+} else {
+    eventCounts = {
+        "user-login": 0,
+        "user-logout": 0,
+        "user-purchase": 0,
+        "profile-update": 0,
+    };
 }
 
+function saveCounts() {
+    fs.writeFileSync(filePath, JSON.stringify(eventCounts, null, 2));
+}
+
+// Event listeners
 emitter.on("user-login", (username) => {
     eventCounts["user-login"]++;
     console.log(`${username} logged in...`);
+    saveCounts();
 });
 
 emitter.on("user-purchase", (username, item) => {
     eventCounts["user-purchase"]++;
     console.log(`${username} purchased ${item} !`);
-})
+    saveCounts();
+});
 
 emitter.on("profile-update", (username, field) => {
     eventCounts["profile-update"]++;
     console.log(`${username} updated their ${field} !`);
-})
+    saveCounts();
+});
 
 emitter.on("user-logout", (username) => {
     eventCounts["user-logout"]++;
     console.log(`${username} logged out`);
-})
+    saveCounts();
+});
 
 emitter.on("summary", () => {
+    console.log("Event Summary:");
     console.log(eventCounts);
-})
+});
 
 // Emit events
 emitter.emit("user-login", "Akhil");
