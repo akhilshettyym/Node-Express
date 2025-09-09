26c6ced072828472024c6afed9c80766b3c5d643
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-09 20:19:13 +0530

    ES Module imports Exports

---
+++ ES_Module/app.js
@@ -0,0 +1,11 @@
+// const math = require("./math");
+// import mult from "./math";
+import { add, subs, mult, div, PI } from "./math.js";
+
+import * as math from "./math.js";
+
+console.log(math.add(5, 10));
+console.log(math.subs(5, 10));
+console.log(math.div(5, 10));
+console.log(math.mult(5, 10));
+console.log(PI);
