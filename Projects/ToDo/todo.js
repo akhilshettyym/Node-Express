ae765219a1afee8f96203dce3580e54a77b573ec
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-09-17 23:28:02 +0530

    Overall comm it

--- Projects/ToDo/todo.js
+++ Projects/ToDo/todo.js
@@ -1,8 +1,8 @@
 import readline from "readline";
 
 const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 const todos = [];
@@ -14,26 +14,26 @@ const showMenu = () => {
     console.log("1. Add a Task ");
     console.log("2. View Tasks ");
     console.log("3. Exit ");
-     console.log("------------------------------------");
+    console.log("------------------------------------");
     rl.question("Choose an option : ", handleInput);
 }
 
 const handleInput = (option) => {
-    if(option === "1") {
+    if (option === "1") {
         rl.question("Enter the task : ", (task) => {
             todos.push(task);
             console.log("Task added : ", task);
             showMenu();
         });
-    } else if(option === "2") {
+    } else if (option === "2") {
         console.log("===================================");
         console.log("\n Your Todo Lists...");
         console.log("===================================");
         todos.forEach((task, index) => {
-            console.log(`${index+1}. ${task}`);
+            console.log(`${index + 1}. ${task}`);
         })
         showMenu();
-    } else if(option === "3") {
+    } else if (option === "3") {
         console.log("------------------------------------");
         console.log("Good bye...");
         console.log("===================================");
