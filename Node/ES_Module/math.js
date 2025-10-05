97e3c31453812ef8a72819fb17fdcbc3feb0cab3
akhilshettyym <akhilshettym2003@gmail.com>
2025-10-05 20:10:00 +0530

    ES Modules

---
+++ Node/ES_Module/math.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subs = (a, b) => {
+    return a - b;
+}
+
+const div = (a, b) => {
+    return a / b;
+}
+
+const mult = (a, b) => {
+    return a * b;
+}
+
+const PI = 3.14;
+
+export default mult;
